43a6f796592f35a2190cf6c72d00c20b09e51a83
KRISHIKUMAR K <87592370+krishikumar22@users.noreply.github.com>
2021-08-05 09:01:22 +0530

    Add files via upload

---
+++ BreakJump.java
@@ -0,0 +1,14 @@
+
+public class BreakJump {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i;
+for(i=0;i<=15;i++) {
+	if(i>=6) {
+	break;
+	}
+System.out.println("the value is "+i);
+	}}
+
+}
